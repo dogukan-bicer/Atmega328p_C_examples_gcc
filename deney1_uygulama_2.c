857583ce52231637d3c19d176e769a640898bc1d
Doğukan Biçer <dogukanbicer52@gmail.com>
2022-03-20 22:31:25 +0300

    Update deney1_uygulama_2.c

--- deney1_uygulama_2.c
+++ deney1_uygulama_2.c
@@ -3,10 +3,11 @@ void main () {
    PORTD=0x0l; // D portuna Hexadecimal 0x0l (Desimal 1) değeri yükleniyor.
 while (1)
  {
+    PORTD=0B00000011;
 while (PORTD<128) // En soldaki bit (MSB) 1 olana kadar dön (10000000 değerine kadar)
   {
-  Delay_ms (200); // 200 milisaniye gecikme
-  PORTD= PORTD << 1; // D portundaki değeri sola bir kaydır
+   Delay_ms (200); // 200 milisaniye gecikme
+   PORTD= PORTD << 1; // D portundaki değeri sola bir kaydır
   }
 while (PORTD>1) // En sağdaki bit (LSB) 1 olana kadar dön (00000001 değerine kadar)
   {
